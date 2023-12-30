e64eac607b5b72c934625b86dba9b39e2f2faf0d
Eduard1710 <eduard.nistor@student.unitbv.ro>
2023-12-30 15:24:48 +0200

    Created project.

---
+++ standaloneClient/src/standaloneClient/Main.java
@@ -0,0 +1,17 @@
+package standaloneClient;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import com.example.ProiectStatelessEjbRemote;
+
+public class Main {
+
+	public static void main(String[] args) throws NamingException{
+		InitialContext context = new InitialContext();
+		
+		ProiectStatelessEjbRemote firstEjb = (ProiectStatelessEjbRemote) context
+				.lookup("java:global/proiectTPDEar/proiectTPDEjb/ProiectStatelessEjb!com.example.ProiectStatelessEjbRemote");
+		firstEjb.insert("Eduard Nistor");
+	}
+}
